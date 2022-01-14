d4eeb4819554097022b4776b38c55e3c5e4c8d79
Dupuy Rony Charles <charlesdupuyrony@charess.org>
2022-01-14 21:12:41 +0100

    only active and pending users are able to connect

--- src/main/java/org/charess/training/service/security/AuthUserService.java
+++ src/main/java/org/charess/training/service/security/AuthUserService.java
@@ -1,6 +1,10 @@
 package org.charess.training.service.security;
 
+import org.charess.training.controller.security.UserController;
+import org.charess.training.domain.security.Status;
 import org.charess.training.repository.security.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,11 +17,15 @@ public class AuthUserService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
+    private final Logger log = LoggerFactory.getLogger(AuthUserService.class);
+
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         org.charess.training.domain.security.User usr = userRepository.findByUsername(username);
-        return new User(usr.getUsername(), usr.getPassword(), usr.getAuthorities());
+        User user = (usr !=null && (usr.getStatus().equals(Status.USER_ACTIVE.toString()) || usr.getStatus().equals(Status.USER_PENDING.toString())))?
+                new User(usr.getUsername(), usr.getPassword(), usr.getAuthorities()):null;
+        return user;
     }
 
     public org.charess.training.domain.security.User findByUsername(String username) {
